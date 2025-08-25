e5c9653bc1a2fae0c3de7f74fd3832bebe594a87
Kennahh <peishenhe@gmail.com>
2025-08-25 15:28:38 +0800

    Add string array to store commands. Add methods to add command and list commands.

--- src/main/java/Chauncey.java
+++ src/main/java/Chauncey.java
@@ -1,8 +1,23 @@
 import java.util.Scanner;
 
 public class Chauncey {
+    private static String[] commands = new String[100];
+    private static int numOfCommands = 0;
+
     public static void printLine(){
         System.out.println("____________________________________________________________");
+    }
+
+    public static void addCommand(String command){
+        commands[numOfCommands] = command;
+        numOfCommands++;
+        System.out.println("added: " + command);
+    }
+
+    public static void listCommands(){
+        for (int i=1; i<=numOfCommands; i++){
+            System.out.println(i + ". " + commands[i-1]);
+        }
     }
 
     public static void main(String[] args) {
@@ -18,7 +33,12 @@ public class Chauncey {
         String command = in.nextLine();
         while (!command.equals("bye")){
             printLine();
-            System.out.println(command);
+            if (command.equals("list")){
+                listCommands();
+            }
+            else{
+                addCommand(command);
+            }
             printLine();
             System.out.println();
             command = in.nextLine();
