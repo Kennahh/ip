859e1a071fc7ae05f61dae59899cb832ce18c4ae
Kennahh <peishenhe@gmail.com>
2025-08-25 14:45:49 +0800

    Add one method to print line and add some comments.

--- src/main/java/Chauncey.java
+++ src/main/java/Chauncey.java
@@ -1,10 +1,18 @@
 public class Chauncey {
-    public static void main(String[] args) {
+    public static void printLine(){
         System.out.println("____________________________________________________________");
+    }
+
+    public static void main(String[] args) {
+        // Add greetings.
+        printLine();
         System.out.println("Hello! I'm Chauncey.");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        printLine();
+
+        // Add exit.
+        printLine();
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        printLine();
     }
 }
