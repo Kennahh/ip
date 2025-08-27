ac20a0c938b5a4736145155c85ae5c2dc2cf5b36
Kennahh <peishenhe@gmail.com>
2025-08-27 23:07:28 +0800

    Add "remove" function in chatbox and make "add" function clearer.

--- src/main/java/Chauncey.java
+++ src/main/java/Chauncey.java
@@ -14,6 +14,15 @@ public class Chauncey {
         System.out.println("added: " + command);
     }
 
+    public static void removeTask(int taskNumber) {
+        String command = tasks[taskNumber-1].getDescription();
+        for (int i=taskNumber-1; i<numOfTask-1; i++){
+            tasks[i] = tasks[i+1];
+        }
+        numOfTask--;
+        System.out.println("removed: " + command);
+    }
+
     public static void listTasks(){
         for (int i = 1; i<= numOfTask; i++){
             System.out.println(i + ".[" + tasks[i-1].getStatusIcon() + "] " + tasks[i-1].getDescription());
@@ -24,6 +33,7 @@ public class Chauncey {
         // Add greetings.
         printLine();
         System.out.println("Hello! I'm Chauncey.");
+        System.out.println("List of things I can do: add / remove / list / mark / unmark ");
         System.out.println("What can I do for you?");
         printLine();
         System.out.println();
@@ -36,6 +46,15 @@ public class Chauncey {
             if (command.equals("list")){
                 listTasks();
             }
+            else if (command.equals("add")) {
+                System.out.print("What task do you want to add? ");
+                String task = in.nextLine();
+                addTask(task);
+            }
+            else if (command.startsWith("remove")) {
+                int taskNumber = Character.getNumericValue(command.charAt(command.length()-1));
+                removeTask(taskNumber);
+            }
             else if (command.startsWith("mark")){
                 int taskNumber = Character.getNumericValue(command.charAt(command.length()-1));
                 tasks[taskNumber-1].markAsDone();
@@ -45,7 +64,7 @@ public class Chauncey {
                 tasks[taskNumber-1].markAsUndone();
             }
             else{
-                addTask(command);
+                System.out.println("Invalid command.");
             }
             printLine();
             System.out.println();
