52b99d7ef0f0001b187becb0c5fef03f156ee46a
Kennahh <peishenhe@gmail.com>
2025-09-08 11:44:14 +0800

    Handle errors caused by incorrect user inputs.

---
+++ src/main/java/ChaunceyException.java
@@ -0,0 +1,13 @@
+public class ChaunceyException extends Exception {
+    public ChaunceyException() {
+        super();
+    }
+
+    public ChaunceyException(String message) {
+        super(message);
+    }
+
+    public ChaunceyException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
